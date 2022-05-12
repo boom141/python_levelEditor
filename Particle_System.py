3705db7b759cf92ff69147943ab7d8a3ffc5d89c
Nijiiro <61643144+boom141@users.noreply.github.com>
2022-05-13 00:34:30 +0800

    Add files via upload

---
+++ Particle_System.py
@@ -0,0 +1,34 @@
+import pygame, sys, random
+
+pygame.init()
+
+Window = pygame.display.set_mode((400,300))
+Fps = pygame.time.Clock()
+
+particles = []
+while 1:
+    Window.fill('black')
+   
+    for particle in particles:
+        particle[0][0] += particle[1][0]
+        particle[0][1] += particle[1][1]
+        particle[2] -= 0.1
+        particle[1][1] += 0.1
+        pygame.draw.circle(Window, (255, 255, 255), [int(particle[0][0]), int(particle[0][1])], int(particle[2]))
+        if particle[2] <= 0:
+            particles.remove(particle)
+  
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_t:
+                for i in range(20):
+                    particles.append([[200, 150], [random.randrange(-3,3),random.randrange(-10,10)], random.randint(4, 6)])
+                    
+
+    pygame.display.update()
+    Fps.tick(60)
